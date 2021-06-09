9d74a1e0cdcee4f844a82b79d7cfe852a26665a6
A B M Asadullah <abmasadullah@gmail.com>
2021-06-10 02:21:06 +0600

    Add card to page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 let myLibrary = [];
+const bookCollectionContainer = document.querySelector(".book-collection-container");
+
+let bookCard = document.createElement("div");
+bookCard.classList.add(".card");
+
+function bookCardBuilder(book){
+  let bookTitle = document.createElement("h3");
+  let bookAuthor = document.createElement("p");
+  let bookPages = document.createElement("p");
+  let bookStatus = document.createElement("p");
+  
+  bookTitle.textContent = `${book.title}`;
+  bookAuthor.textContent = `${book.author}`;
+  bookTitle.textContent = `${book.pages}`;
+  bookTitle.textContent = `${book.read? 'already read': 'not read yet'}`;
+}
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -19,6 +35,10 @@ const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
 const greatGatsby = new Book('The Great Gatsby', 'Author 1', 400, true)
 const mobyDick = new Book('Moby Dick', 'Author 2', 500, false)
 console.log(theHobbit.info()) // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+
+bookCard.appendChild(bookCardBuilder(theHobbit))
+bookCollectionContainer.appendChild(bookCard)
+
 
 addBookToLibrary(theHobbit)
 addBookToLibrary(greatGatsby)
