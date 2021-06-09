369fadb124e6a80dce8be323dbe657ab5fe47926
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-06-09 11:54:38 -0600

    create function addBookToLibrary

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.info = function() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read? 'already read': 'not read yet'}.`
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+const theHobbit = new Book('The Great Gatsby', 'Author 1', 400, true)
+const theHobbit = new Book('Moby Dick', 'Author 2', 500, false)
+console.log(theHobbit.info()) // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+
+addBookToLibrary(theHobbit)
+console.log(myLibrary) // hobbit book obj
