733f99f6a689a7acdea472b8a332c659c0fb0077
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-06-09 14:43:52 -0600

    create function to add books in myLirbary to the page

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 let myLibrary = [];
 const bookCollectionContainer = document.querySelector(".book-collection-container");
 
-let bookCard = document.createElement("div");
-bookCard.classList.add(".card");
-
 function bookCardBuilder(book){
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("card");
   let bookTitle = document.createElement("h3");
   let bookAuthor = document.createElement("p");
   let bookPages = document.createElement("p");
   let bookStatus = document.createElement("p");
-  
-  bookTitle.textContent = `${book.title}`;
-  bookAuthor.textContent = `${book.author}`;
-  bookTitle.textContent = `${book.pages}`;
-  bookTitle.textContent = `${book.read? 'already read': 'not read yet'}`;
+
+  bookTitle.textContent = `Title: ${book.title}`;
+  bookAuthor.textContent = `Author: ${book.author}`;
+  bookPages.textContent = `Pages: ${book.pages}`;
+  bookStatus.textContent = `Status: ${book.read? 'already read': 'not read yet'}`;
+
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookPages);
+  bookCard.appendChild(bookStatus);
+  bookCollectionContainer.appendChild(bookCard);
 }
 
 function Book(title, author, pages, read) {
@@ -34,14 +39,12 @@ function addBookToLibrary(book) {
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
 const greatGatsby = new Book('The Great Gatsby', 'Author 1', 400, true)
 const mobyDick = new Book('Moby Dick', 'Author 2', 500, false)
-console.log(theHobbit.info()) // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-
-bookCard.appendChild(bookCardBuilder(theHobbit))
-bookCollectionContainer.appendChild(bookCard)
-
 
 addBookToLibrary(theHobbit)
 addBookToLibrary(greatGatsby)
 addBookToLibrary(mobyDick)
 
-console.log(myLibrary) // hobbit book obj
+myLibrary.forEach(function(book) {
+  bookCardBuilder(book);
+});
+
