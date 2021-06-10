b85eedd24df5138fd3257e9761851b10517ab9b7
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-06-10 17:50:40 -0600

    add new book event now works properly. An error message is displayed if user didn't fill out all fields of the form

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
-let myLibrary = [];
-const bookCollectionContainer = document.querySelector(".book-collection-container");
-const addBookButton = document.querySelector(".add-book-btn");
-const titleInput = document.querySelector("#title");
-const authorInput = document.querySelector("#author");
-const pagesInput = document.querySelector("#pages");
+const myLibrary = [];
+const bookCollectionContainer = document.querySelector('.book-collection-container');
+const addBookButton = document.querySelector('.add-book-btn');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
 const statusInput = document.querySelector("input[name='status']");
+const feedbackMessage = document.createElement('div');
+const modalHeader = document.querySelector('.modal-header');
+const modalTitle = document.querySelector('.modal-title');
+feedbackMessage.textContent = 'Please check all the fields are complete.';
+feedbackMessage.style.width = '100%';
+feedbackMessage.classList.add('text-center', 'bg-danger');
 
-function bookCardBuilder(book){
-  let bookCard = document.createElement("div");
-  bookCard.classList.add("card", "m-2", "card-width", "bg-light");
-  let bookTitle = document.createElement("h3");
-  bookTitle.classList.add("text-center", "card-header");
-  let cardBody = document.createElement("div");
-  cardBody.classList.add("card-body");
-  let bookAuthor = document.createElement("p");
-  let bookPages = document.createElement("p");
-  let bookStatus = document.createElement("p");
-  bookStatus.classList.add("card-footer", "m-0")
+function bookCardBuilder(book) {
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('card', 'm-2', 'card-width', 'bg-light');
+  const bookTitle = document.createElement('h3');
+  bookTitle.classList.add('text-center', 'card-header');
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  const bookAuthor = document.createElement('p');
+  const bookPages = document.createElement('p');
+  const bookStatus = document.createElement('p');
+  bookStatus.classList.add('card-footer', 'm-0');
 
   bookTitle.textContent = `Title: ${book.title}`;
   bookAuthor.textContent = `Author: ${book.author}`;
   bookPages.textContent = `Pages: ${book.pages}`;
-  bookStatus.textContent = `Status: ${book.read? 'already read': 'not read yet'}`;
+  bookStatus.textContent = `Status: ${book.read ? 'already read' : 'not read yet'}`;
 
   bookCard.appendChild(bookTitle);
   cardBody.appendChild(bookAuthor);
@@ -38,9 +44,9 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.info = function() {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read? 'already read': 'not read yet'}.`
-}
+Book.prototype.info = function info() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'already read' : 'not read yet'}.`;
+};
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -52,41 +58,36 @@ const mobyDick = new Book('Moby Dick', 'Author 2', 500, false);
 const harryPotter = new Book('J. K. Rowling', 'Author 4', 600, true);
 const harryPotterr = new Book('J. K. Rowling', 'Author 4', 600, true);
 
-addBookToLibrary(theHobbit)
-addBookToLibrary(greatGatsby)
-addBookToLibrary(mobyDick)
-addBookToLibrary(harryPotter)
-addBookToLibrary(harryPotterr)
+addBookToLibrary(theHobbit);
+addBookToLibrary(greatGatsby);
+addBookToLibrary(mobyDick);
+addBookToLibrary(harryPotter);
+addBookToLibrary(harryPotterr);
 
-myLibrary.forEach(function(book) {
+myLibrary.forEach((book) => {
   bookCardBuilder(book);
 });
 
 // Add book button
 
 function validateInput() {
-  return titleInput.value && authorInput.value && pagesInput.value && statusInput.value
+  return titleInput.value && authorInput.value && pagesInput.value && statusInput.value;
 }
 
 function addBookBtn() {
   if (validateInput()) {
-    if (typeof feedbackMessage != "undefined" && feedbackMessage) {
+    if (feedbackMessage.parentNode === modalHeader) {
       feedbackMessage.remove();
     }
-    let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, statusInput.value);
+    const newBook = new Book(titleInput.value, authorInput.value,
+      pagesInput.value, statusInput.value);
     bookCardBuilder(newBook);
   } else {
-    if (typeof feedbackMessage != "undefined" && feedbackMessage){ return}
-    feedbackMessage = document.createElement('div');
-    feedbackMessage.textContent = "Please check all the fields are complete.";
-    feedbackMessage.style.width = "100%";
-    feedbackMessage.classList.add("text-center", "bg-danger")
-    modalHeader = document.querySelector(".modal-header");
-    modalTitle = document.querySelector(".modal-title");
+    if (feedbackMessage.parentNode === modalHeader) {
+      return;
+    }
     modalHeader.insertBefore(feedbackMessage, modalTitle);
-    
   }
-
 }
 
-addBookButton.addEventListener('click', addBookBtn)
+addBookButton.addEventListener('click', addBookBtn);
